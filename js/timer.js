8dde5b1dca07d5f2c32429bf4edfbbd55a2f2059
Sufmik <74490460+Sufmik@users.noreply.github.com>
2022-03-20 13:13:36 +0300

    0,5

---
+++ js/timer.js
@@ -0,0 +1,29 @@
+const timerBlock = document.querySelector('.timer__time')
+const deadLine = '31 march 2022'
+let interval
+
+const updateClock = () => {
+    const date = new Date().getTime()
+    const dateDeadLine = new Date(deadLine).getTime()
+
+    const timeRemaining = (dateDeadLine - date) / 1000
+    const days = Math.floor(timeRemaining / 60 / 60 / 24)                       //УСЛОЖНЕННОЕ ЗАДАНИЕ ДЕНЬ 3
+    const hours = Math.floor((timeRemaining / 60 / 60) % 24)                    //УСЛОЖНЕННОЕ ЗАДАНИЕ ДЕНЬ 3
+    const minutes = Math.floor((timeRemaining / 60) % 60)
+    const seconds = Math.floor(timeRemaining % 60)
+
+    const fHours = hours < 10 ? '0' + hours : hours
+    const fMinutes = minutes < 10 ? '0' + minutes : minutes
+    const fSeconds = seconds < 10 ? '0' + seconds : seconds
+
+    timerBlock.textContent = `${days}:${fHours}:${fMinutes}:${fSeconds}`        //УСЛОЖНЕННОЕ ЗАДАНИЕ ДЕНЬ 3
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval)
+        timerBlock.textContent = `00:00:00`
+    }
+}
+
+updateClock()
+
+interval = setInterval(updateClock, 500)
